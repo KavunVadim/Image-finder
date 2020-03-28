832810c665b530e967e257c3315d6c8118e28514
Diz1111Diz <kavunv@gmail.com>
2020-03-28 20:35:47 +0200

    fix hw-13

--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,21 @@
 'use strict';
 import apiService from '../js/apiService.js';
-import templates from '../templates/templat-item-cards.hbs';
+import templatesCard from '../templates/templat-item-cards.hbs';
+import templatSearchForm from '../templates/templat-search-form.hbs';
+import templatGalleryList from '../templates/templat-gallery-list.hbs';
+import templatBtn from '../templates/templat-btn.hbs';
 
-class Form {
+class SearchImages {
     constructor() {
         this.container = document.querySelector('body');
         this.searchForm = null;
         this.galleryList = null;
         this.btn = null;
-
-        this.start();
     }
 
-    renderSearchForm = () => {
-        return `
-        <form class="search-form" id="search-form">
-        <input
-        class="search-input"
-          type="text"
-          name="query"
-          autocomplete="off"
-          placeholder="Search images..."
-        />
-      </form>`;
-    };
-    renderImgList = () => {
-        return `<ul class="gallery card-list js-card-list"></ul>`;
-    };
-    renderBtn = () => {
-        return `<button type="button" class = "load-btn">Load more</button>`;
-    };
-
     start = () => {
-        this.addToScreen(this.container, 'beforeend', this.renderSearchForm());
-        this.addToScreen(this.container, 'beforeend', this.renderImgList());
+        this.addToScreen(this.container, 'beforeend', templatSearchForm());
+        this.addToScreen(this.container, 'beforeend', templatGalleryList());
 
         this.searchForm = document.querySelector('#search-form');
         this.searchInput = document.querySelector('.search-input');
@@ -48,51 +30,55 @@ class Form {
 
     searchImg = e => {
         e.preventDefault();
-        this.clear();
-        if (this.btn) this.btn.remove();
+        this.clearList();
+        this.removeBtn();
+
         apiService.changeWord(this.searchInput.value);
         apiService.getPicture();
-        this.render();
+        this.renderImg();
     };
 
-    render = async() => {
+    renderImg = async() => {
         this.length = this.galleryList.clientHeight;
-        const api = await apiService.getPicture();
-        const render = this.build(api);
+        const arrImg = await apiService.getPicture();
+        const render = this.renderMarkup(arrImg);
 
-        if (api.length) {
-            this.addToScreen(this.container, 'beforeend', this.renderBtn());
+        if (arrImg.length) {
+            this.addToScreen(this.container, 'beforeend', templatBtn());
             this.btn = document.querySelector('.load-btn');
             this.btn.addEventListener('click', this.loadMoreBtn);
-            return;
         }
         return render;
     };
 
     scroll = () => {
         window.scrollTo({
-            top: this.length + 90,
+            top: this.length,
             behavior: 'smooth',
         });
     };
 
-    build = items => {
-        const markup = items.reduce((acc, el) => templates(el) + acc, '');
-        this.galleryList.insertAdjacentHTML('beforeend', markup);
-    };
-
-    clear = () => {
-        this.galleryList.innerHTML = '';
+    renderMarkup = items => {
+        const markup = items.reduce((acc, el) => templatesCard(el) + acc, '');
+        this.addToScreen(this.galleryList, 'beforeend', markup);
     };
 
     loadMoreBtn = async() => {
+        this.removeBtn();
+        apiService.nextPage();
+        const nextRender = await this.renderImg();
+        this.scroll();
+    };
+
+    removeBtn = () => {
         if (this.btn) {
             this.btn.remove();
-            apiService.nextPage();
         }
-        const nextRender = await this.render();
-        this.scroll();
+    };
+
+    clearList = () => {
+        this.galleryList.innerHTML = '';
     };
 }
 
-new Form();
+new SearchImages().start();
